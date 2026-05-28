900f0f3b6b734dc89eb428a63c50a952963b505b
kosuke-17 <tamutamu17@icloud.com>
2026-05-28 20:57:29 +0900

    add: aiを試しに実行

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { createOpenAI } from "./src/providers/openai";
+import { generateText } from "./src/core/generate-text";
+import type { Message } from "./src/type";
+
+const messages: Message[] = [
+  { role: "user", content: "AIエージェントとは何ですか？" },
+];
+
+const openai = createOpenAI();
+
+const model = openai("gpt-4o-mini");
+const result = await generateText({ model, messages });
+
+console.log(result);
